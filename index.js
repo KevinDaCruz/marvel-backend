3ddeb6439df1870d032a0d18b666458e1505bb9f
Kevin Da Cruz <kevindacruz@Air-de-Lea.lan>
2024-04-05 01:31:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/characters", async (req, res) => {
+  try {
+    const name = req.query.name || "";
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${name}&skip=${skip}&limit=${limit}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const title = req.query.title || "";
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&title=${title}&skip=${skip}&limit=${limit}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  console.log("Je suis bien dans la route all");
+  res.status(404).json({ message: "Page not found !" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started 🚀");
+});
